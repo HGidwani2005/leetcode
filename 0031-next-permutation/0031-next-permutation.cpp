87efd220e5a0f79f664a5fd00032c027c3091b8f
Himanshu Gidwani <148280361+HGidwani2005@users.noreply.github.com>
2026-07-15 03:25:00 +0530

    Time: 0 ms (100%), Space: 15.8 MB (40.47%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
         int idx=-1;
+        int n=nums.size();
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 idx=i;
@@ -14,9 +14,6 @@ public:
             return;
         }
         reverse(nums.begin()+idx+1,nums.end());
-
-
-        //finding the just greater element than idx
         int j=-1;
         for(int i=idx+1;i<n;i++){
             if(nums[i]>nums[idx]){
@@ -24,6 +21,7 @@ public:
                 break;
             }
         }
+        
         int temp=nums[idx];
         nums[idx]=nums[j];
         nums[j]=temp;
